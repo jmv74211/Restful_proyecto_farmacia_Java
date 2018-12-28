290b259425edada8921eb4c6164ce3f57ad40715
Jonathan <jmv74211@gmail.com>
2018-12-28 14:14:25 +0100

    Añade el modelo de producto

---
+++ src/main/java/jmv74211/DSS_P4/models/Product.java
@@ -0,0 +1,128 @@
+package jmv74211.DSS_P4.models;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Product")
+public class Product implements Serializable  {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "productId")
+	private int productId;
+	
+	@ManyToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name="departmentId", nullable=false)
+    
+	private Department department;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "imageURL")
+	private String imageURL;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "outstanding")
+	private boolean outstanding;
+	
+	@Column(name = "price")
+	private float price;
+
+	public Product(){
+		
+	}
+	
+	public Product(int productId, Department department, String name, String imageURL, String description,
+				boolean outstanding, float price) {
+		
+		this.productId = productId;
+		this.department = department;
+		this.name = name;
+		this.imageURL = imageURL;
+		this.description = description;
+		this.outstanding = outstanding;
+		this.price = price;
+	}
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImageURL() {
+		return imageURL;
+	}
+
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isOutstanding() {
+		return outstanding;
+	}
+
+	public void setOutstanding(boolean outstanding) {
+		this.outstanding = outstanding;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", department=" + department + ", name=" + name + ", imageURL="
+				+ imageURL + ", description=" + description + ", outstanding=" + outstanding + ", price=" + price + "]";
+	}
+	
+	
+	
+	
+
+}
